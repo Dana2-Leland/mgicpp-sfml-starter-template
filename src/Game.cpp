1b137819e7fa7e086f550051186ee2011a8ea93c
Dana2-Leland <Dana2.Leland@live.uwe.ac.uk>
2025-10-24 14:59:10 +0100

    update2
    invalid passports

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,38 +23,38 @@ bool Game::init()
 	std::random_device rd;  // a seed source for the random number engine
 	std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
 	//std::uniform_int_distribution<> distrib(0, 8);
-	std::uniform_int_distribution<int> distribution_1_8(1,8);
+	std::uniform_int_distribution<int> distribution_1_8(0,8);
 	int randEntry = distribution_1_8(rd);
 
 
 	std::uniform_int_distribution<int> distribution_0_1(0, 1);
-
 	int torf = distribution_0_1(rd);
-	if (torf == 1) { isValid = true; } //create 50/50 chance of passport being valid
+	//torf = 0; //debug line
 
+	if (torf == 1) { 
+		isValid = true; } //create 50/50 chance of passport being valid
 
-	randEntry = 2; //delete later
 	std::vector<std::string> animalNames = { "buffalo", "chicken", "elephant", "giraffe", "gorilla", "moose", "narwhal", "penguin", "walrus" }; //load all animal names into vector
 
 	//load character texture
-	if (!character_texture.loadFromFile("../Data/Images/Critter Crossing Customs/" + animalNames[randEntry] + ".png")) {
+	if (!character_texture.loadFromFile("../Data/Images/Critter Crossing Customs/icons/" + animalNames[randEntry] + ".png")) {
 		std::cout << "character texture could not be loaded";
 	}
 	character->setTexture(character_texture);
 	character->setPosition(100, 100); //set scalable position later
 	
-	//load passport texture
-	if (isValid && !passport_texture.loadFromFile("../Data/Images/Critter Crossing Customs/" + animalNames[randEntry] + " passport.png")) {
-		//only elephant or penguin currently have existing passports
-		//"D:\Dana\mgicpp-sfml-starter-template\Data\Images\Critter Crossing Customs\elephant passport.png"
+	//load valid passport texture
+	if (isValid && !passport_texture.loadFromFile("../Data/Images/Critter Crossing Customs/validPassports/" + animalNames[randEntry] + " passport.png")) {
 		std::cout << "passport texture could not be loaded";
 	}
-	//add valid passports for each animal in file. 
-	//add boolean for isValidPassport, select randomly whether true or not, and create invalid passport folder
 
+	//generate invalid passport
+	if (!isValid && !passport_texture.loadFromFile("../Data/Images/Critter Crossing Customs/invalidPassports/" + animalNames[randEntry] + "_invalid passport0.png")) {
+		std::cout << "invalid passport texture could not be loaded";
+	}
 	passport->setTexture(passport_texture);
 	passport->setPosition(500, 50); //set scalable position later
-	passport->setScale(.7,.7);
+	passport->setScale(.7, .7);
 
 
 
